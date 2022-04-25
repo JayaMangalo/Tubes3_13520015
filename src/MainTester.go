1eec52256669079f9481c24f90223463e50128c0
Raven <16520022@std.stei.ac.id>
2022-04-25 19:54:48 +0700

    FINISH REGEX

--- src/MainTester.go
+++ src/MainTester.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-	"log"
-	"main/src/handler"
-	"net/http"
+	// "log"
+	// "main/src/handler"
+	// "net/http"
 )
 
 // import "fmt"
 
 func sanitize() {}
 
-func main() {
-	/*
-		text, err := ReadFile("disease/Covid.txt")
-		text2, err2 := ReadFile("people/Gerald.txt")
+// func main() {
+// 	/*
+// 		text, err := ReadFile("disease/Covid.txt")
+// 		text2, err2 := ReadFile("people/Gerald.txt")
 
-		if err == false || err2 == false {
-			return
-		}
-		if isSanitizedRegex(text) == false || isSanitizedRegex(text2) == false {
-			return
-		}
+// 		if err == false || err2 == false {
+// 			return
+// 		}
+// 		if isSanitizedRegex(text) == false || isSanitizedRegex(text2) == false {
+// 			return
+// 		}
 
-		var index int = LCSPercentage(text2, text)
-		println(index)
-	*/
+// 		var index int = LCSPercentage(text2, text)
+// 		println(index)
+// 	*/
 
-	// TES NGAMBIL DATA DARI DATABASE
-	//connector.GetDataOrang()
+// 	// TES NGAMBIL DATA DARI DATABASE
+// 	//connector.GetDataOrang()
 
-	mux := http.NewServeMux()
+// 	mux := http.NewServeMux()
 
-	// handle route homepage(root)
-	mux.HandleFunc("/", handler.HomeHandler)
-	mux.HandleFunc("/inputPenyakit", handler.GetInputPenyakitHandler)
-	mux.HandleFunc("/processInput", handler.PostPenyakitHandler)
-	mux.HandleFunc("/lihatDiagnosis", handler.DiagnosisHandler)
+// 	// handle route homepage(root)
+// 	mux.HandleFunc("/", handler.HomeHandler)
+// 	mux.HandleFunc("/inputPenyakit", handler.GetInputPenyakitHandler)
+// 	mux.HandleFunc("/processInput", handler.PostPenyakitHandler)
+// 	mux.HandleFunc("/lihatDiagnosis", handler.DiagnosisHandler)
 
-	log.Println("Starting web on port 8080")
+// 	log.Println("Starting web on port 8080")
 
-	err5 := http.ListenAndServe(":8080", mux)
-	log.Fatal(err5)
+// 	err5 := http.ListenAndServe(":8080", mux)
+// 	log.Fatal(err5)
 
-}
+// }
---
+++ src/struct.go
@@ -0,0 +1,13 @@
+package main
+
+type SearchRegex struct {
+	day string 
+	month string
+	year string
+	disease string
+}
+
+type RegexFormat struct{
+	regex string
+	splitter string
+}
