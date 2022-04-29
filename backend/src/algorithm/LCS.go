a519eb07481492749517975a2ec6e109c6c4817d
Raven <16520022@std.stei.ac.id>
2022-04-29 20:47:49 +0700

    better LCS algo

--- backend/src/algorithm/LCS.go
+++ backend/src/algorithm/LCS.go
@@ -3,20 +3,32 @@ package algorithm
 //fungsi bantuan yang digunakan untuk mencari kemiripan pattern 
 //dengan text dalam bentuk integer persentase yang direturn
 func LCSPercentage(text, pattern string) int {
-	same := LCS(text, pattern,len(text),len(pattern),0)
+	same := LCS(text, pattern)
 	percentage := same * 100 / len(pattern)
 	return percentage
 }
-
-//fungsi rekursif Least Common Substring, me return panjang substring terbesar
-func LCS(X, Y string, i,j,count int) int{
-	if (i == 0 || j == 0){
-		return count;
+	
+//fungsi LCS untuk mencari least common substring
+func LCS(text,pattern string) int {
+	m := len(text)
+	n := len(pattern)
+	L := make([][]int, m+1)   
+	
+	var length int 
+	length = 0
+	for i := 0; i < m+1; i ++ {
+		L[i] = make([]int, n+1)  
+			for j := 0; j < n+1; j++ {
+				if i == 0 || j == 0 {
+					L[i][j] = 0;
+					} else if text[i-1] == pattern[j-1] {
+						L[i][j] = L[i-1][j-1] + 1
+						length = max(L[i][j],length);
+					} else {
+				L[i][j] = 0;
+			}
+		}
 	}
-
-	if (X[i - 1] == Y[j - 1]){
-		count = LCS(X,Y,i - 1, j - 1, count + 1);
+	
+	return length
 	}
-	count = max(count,max(LCS(X,Y, i, j - 1, 0),LCS(X,Y,i - 1, j, 0)));
-	return count;
-    }
